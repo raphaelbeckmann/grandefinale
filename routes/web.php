5233af47f5c79ce24ff6330c2cdfa0439b28d04f
Raphael Beckmann <raphael@beckmann.io>
2019-11-20 23:21:03 +0100

    Add API routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,13 @@ Route::post('create', 'KundenController@create')->name('create');
 Route::get('show/{kunde}', 'KundenController@show')->name('show');
 Route::get('list', 'KundenController@list')->name('list');
 
+
+// MVC-API-Lösung
+Route::post('create', 'KundenApiController@create')->name('create');
+Route::get('show/{kunde}', 'KundenApiController@show')->name('show');
+Route::get('list', 'KundenApiController@list')->name('list');
+
+
 // Musterlösung
 Route::any('musterloesung', function() {
     return view('musterloesung');
