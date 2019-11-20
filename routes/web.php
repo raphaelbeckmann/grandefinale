b154eacf6ee283b4ef2350ae32d2345f4044b859
Raphael Beckmann <raphael@beckmann.io>
2019-11-20 23:21:33 +0100

    Change routes order

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Musterlösung
+Route::any('musterloesung', function () {
+    return view('musterloesung');
+});
+
+
 // MVC-Lösung
 Route::get('form', 'KundenController@form')->name('form');
 Route::post('create', 'KundenController@create')->name('create');
@@ -13,9 +19,3 @@ Route::get('list', 'KundenController@list')->name('list');
 Route::post('create', 'KundenApiController@create')->name('create');
 Route::get('show/{kunde}', 'KundenApiController@show')->name('show');
 Route::get('list', 'KundenApiController@list')->name('list');
-
-
-// Musterlösung
-Route::any('musterloesung', function() {
-    return view('musterloesung');
-});
