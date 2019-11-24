a57db3b45a6e6736133e43a936ee9c07bd01f70b
Amadeus Beckmann <amadeus@beckmann.io>
2019-11-24 05:26:55 +0100

    Add react setup

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+
+render(
+  <App/>,
+  document.getElementById('root'),
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
